dbafe33df5d51bb0174de5c018e1cc0f5680d503
esmaeylulkayikci <esmaeylulkayikci@gmail.com>
2026-04-15 12:33:32 +0300

    LAB-5

--- LAB-2/main.cpp
+++ LAB-2/main.cpp
@@ -1,95 +1,38 @@
 #include <iostream>
-
+#include <cstdio>
+#include <cstdlib>
 using namespace std;
-
-struct Node
-{
-    string data;
-    Node* next;
-};
-
-void addword(Node*& head, string word)
-{
-    Node* new_node = new Node();
-    new_node -> data = word;
-    new_node -> next = nullptr;
-
-    if (head == nullptr)
-    {
-        head = new_node;
-    }
-    else
-    {
-        Node* temp = head;
-        while (temp -> next != nullptr)
-        {
-            temp = temp -> next;
-        }
-        temp -> next = new_node;
-    }
-}
-
-void undo(Node*& head)
-{
-    if (head == nullptr)
-    {
-        return;
-    }
-
-    if (head -> next == nullptr)
-    {
-        delete head;
-        head = nullptr;
-        return;
-    }
-
-    Node* temp = head;
-    while (temp -> next -> next != nullptr)
-    {
-        temp = temp -> next;
-    }
-    delete temp -> next;
-    temp -> next = nullptr;
-}
-
-void display(Node* head)
-{
-    Node* temp = head;
-    while (temp != nullptr)
-    {
-        cout << "[" << temp -> data << "] -> ";
-        temp = temp -> next;
-    }
-    cout << "\nNULL\n" << endl;
-}
 
 int main()
 {
-    Node* head = nullptr;
-    string input;
+    FILE *fp = NULL;
+    fp = fopen{"/etc/passwd" , "r"};
 
-    cout << "\nBasic Text Reviser\n" << endl;
-    cout << "Enter a word, type erasing for 'UNDO', exit to 'EXIT'\n" << endl;
-
-    while (true)
+    if (fp == NULL)
     {
-        cout << "> ";
-        cin >> input;
-
-        if(input == "EXIT")
-        {
-            break;
-        }
-        else if (input == "UNDO")
-        {
-            undo(head);
-        }
-        else
-        {
-            addword(head, input);
-        }
-        display (head);
+        perror("fopen");
+       /* exit(1); */
     }
+    else
+    {
+        fclose(fp);
+    }
+
+/*    fp = fopen("C:\Project\file.txt" , "r");    */ 
+    fp = fopen("C:\\Project\\file.txt" , "r");
+
+    if (fp != NULL)
+    {
+        fclose(fp);
+    }
+    
+    fp = fopen("file.txt" , "r");
+
+    if (fp != NULL)
+    {
+        fclose(fp);
+    }
+    
 
     return 0;
 }
